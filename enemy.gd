0937e567e46c76213e8392e830f3982b0f9e6ee8
dpanjagala <panjagaladivya03@gmail.com>
2025-05-18 09:22:25 +0530

    Added Enemy

---
+++ enemy.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+var speed = 40
+var player_chase = false
+var player = null
+
+func _physics_process(delta):
+	if player_chase and player:
+		var direction = (player.position - position).normalized()
+		velocity = direction * speed
+		move_and_slide()
+
+		$AnimatedSprite2D.play("walk")
+
+		if player.position.x - position.x < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		$AnimatedSprite2D.play("idle")
+
+func _on_detection_area_body_entered(body):
+	print("Detected: ", body.name)
+	player = body
+	player_chase = true
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
