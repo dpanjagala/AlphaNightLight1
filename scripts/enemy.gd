ddc88ee5558f98cdf5801e3d7528c857bdbaab0f
dpanjagala <panjagaladivya03@gmail.com>
2025-05-22 06:16:30 +0530

    An additional commit for alpha

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -8,7 +8,6 @@ var health = 100
 var player_inattack_zone = false
 
 func _physics_process(delta):
-	deal_with_damage()
 	
 	if player_chase and player:
 		var direction = (player.position - position).normalized()
@@ -42,10 +41,3 @@ func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
 func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_inattack_zone = false
-
-func deal_with_damage():
-	if player_inattack_zone and Global.player_current_attack == true:
-		health -= 20
-		print("slime health = ", health)
-		if health <= 0:
-			self.queue_free()
